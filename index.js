81281630f4ee5f52e6a6cd639913e370550e4070
CaitlinCodes <caiit@me.com>
2018-08-22 14:03:30 -0600

    index

---
+++ index.js
@@ -0,0 +1,20 @@
+let express = require('express')
+let bp = require('body-parser')
+// require ('./dbconfig/db-config')
+let port = 3000
+let server = express()
+server.use(bp.json())
+server.use(bp.urlencoded({
+  extended: true
+}))
+
+
+
+
+
+
+
+
+server.listen(port, () => {
+  console.log('Listening on port', port)
+})
