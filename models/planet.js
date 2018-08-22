f2dde075b2a0831a6f8aa243e3e006b148a9eb8f
CaitlinCodes <caiit@me.com>
2018-08-22 15:39:31 -0600

    model work

---
+++ models/planet.js
@@ -0,0 +1,38 @@
+let mongoose = require('mongoose')
+let Schema = mongoose.Schema
+let ObjectId = Schema.Types.ObjectId
+let schemaName = 'Planet'
+
+
+let schema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    default: 'No description included'
+  },
+  color: {
+    type: String,
+    enum: [
+      'red',
+      'yellow',
+      'blue',
+      'orange',
+      'white',
+      'black'
+    ],
+    required: true
+  },
+
+  galaxyId: {
+    type: ObjectId,
+    ref: 'Galaxy',
+    required: true
+  }
+
+
+})
+
+module.exports = mongoose.model(schemaName, schema)
